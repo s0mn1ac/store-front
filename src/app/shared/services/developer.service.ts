6d3b8a663eb58325c353d2ff3a4d4b3732dd4533
Juan Miguel González Machado <juanmi.gonzalezm@gmail.com>
2021-05-07 20:30:42 +0100

    add: servicio para gestionar las desarrolladoras

---
+++ src/app/shared/services/developer.service.ts
@@ -0,0 +1,72 @@
+import { HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Developer } from 'src/app/layout/components/developer/models/developer.model';
+import { BaseService } from './common/base.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeveloperService extends BaseService {
+
+    private url = 'http://localhost:8090/developer';
+    private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+    public getAllDevelopers(): Promise<Developer[]> {
+        return this.serviceGet({
+            url: this.url,
+            headers: this.httpHeaders,
+            callback: (response: any) => this.convertDevelopersFromReport(response?.body),
+            result: null
+        });
+    }
+
+    public addDeveloper(developer: Developer): Promise<Developer[]> {
+        return this.servicePost({
+            url: this.url,
+            params: developer,
+            headers: this.httpHeaders,
+            callback: (response: any) => response?.body,
+            result: null
+        });
+    }
+
+    public modifyDeveloper(developer: Developer): Promise<Developer[]> {
+        return this.servicePut({
+            url: `${this.url}/${developer.id}`,
+            params: developer,
+            headers: this.httpHeaders,
+            callback: (response: any) => response?.body,
+            result: null
+        });
+    }
+
+    public deleteDeveloper(developer: Developer): Promise<Developer[]> {
+        return this.serviceDelete({
+            url: `${this.url}/${developer.id}`,
+            headers: this.httpHeaders,
+            callback: (response: any) => response?.body,
+            result: null
+        });
+    }
+
+    private convertDevelopersFromReport(report: any[]): Developer[] {
+
+        const developers: Developer[] = [];
+    
+        report?.forEach((reportItem: any) => {
+    
+          const developer: Developer = new Developer();
+    
+          developer.id = reportItem.id;
+          developer.name = reportItem.name;
+          developer.cif = reportItem.cif;
+    
+          developers.push(developer);
+    
+        });
+    
+        return developers;
+
+    }
+
+}
